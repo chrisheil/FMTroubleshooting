5099518759e83ae053f7e995a2aae093d670343d
Chris Heil <chris.heil@urbanft.com>
2019-12-12 13:06:25 -0600

    Initial Commit

---
+++ FMTroubleshooting/Migrations/Initial/InitialSchemaCreation.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMTroubleshooting.Migrations.Initial
+{
+    [Migration(1)]
+    public sealed class InitialSchemaCreation : Migration
+    {
+        public override void Down() => throw new NotImplementedException();
+
+        public override void Up()
+        {
+            Execute.EmbeddedScript("InitialSchema.sql");
+        }
+    }
+}
---
+++ FMTroubleshooting/Program.cs
@@ -0,0 +1,44 @@
+﻿using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.SqlServer;
+
+namespace FMTroubleshooting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var announcer = new ConsoleAnnouncer();
+
+            // Processor specific options (usually none are needed)
+            var options = new ProcessorOptions();
+
+            var processorFactory = new SqlServerProcessorFactory();
+
+            // Initialize the processor
+            using (var processor = processorFactory.Create(
+                "server=(local);database=testDatabase;user=testUser;password=testPassword",
+                announcer,
+                options))
+            {
+                // Configure the runner
+                var context = new RunnerContext(announcer)
+                {
+                    ApplicationContext = "some value"
+                };
+
+                // Create the migration runner
+                var runner = new MigrationRunner(
+                    // Specify the assembly with the migrations
+                    typeof(Program).Assembly,
+                    context,
+                    processor);
+
+                // Run the migrations
+                runner.MigrateUp();
+            }
+        }
+    }
+}
